2d5380b699c8526bc0b8eb64a250433b1d45408b
Mani Rastegari <mani.rastegari@gmail.com>
2023-05-19 09:26:09 -0400

    passwordEncryptionMongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -10,6 +12,21 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+mongoose.connect("mongodb+srv://mani16032:mani16032@cluster0.idomekg.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -23,6 +40,49 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+  .then(() => {
+    res.render("secrets");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username })
+  .then((foundUser) => {
+    if (foundUser && foundUser.password === password) {
+      res.render("secrets");
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+    // User.findOne({email: username}, function(err, foundUser){
+    //     if(err) {
+    //         console.log(err);
+    //     } else {
+    //         if(foundUser){
+    //             if(foundUser.password === password){
+    //                 res.render("secrets");
+    //             }
+    //         }
+    //     }
+    // });
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
